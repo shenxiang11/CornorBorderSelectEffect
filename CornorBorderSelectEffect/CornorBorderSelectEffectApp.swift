085e0e11693d85eac5c310450156c7cebb6f066e
Shen <863461783@qq.com>
2024-03-08 19:30:43 +0800

    Initial Commit

---
+++ CornorBorderSelectEffect/CornorBorderSelectEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornorBorderSelectEffectApp.swift
+//  CornorBorderSelectEffect
+//
+//  Created by 香饽饽zizizi on 2024/3/8.
+//
+
+import SwiftUI
+
+@main
+struct CornorBorderSelectEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
